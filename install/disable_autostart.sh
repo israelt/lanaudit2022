29e354952b6939a10d36573892b55242ff0a4bf2
Israel Torres <israel.torres.gonzalo@gmail.com>
2022-05-26 01:22:43 +0200

    Update disable_autostart.sh
    Update to 2022 Q2 version

--- install/disable_autostart.sh
+++ install/disable_autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # EJECUTAR CON PERMISOS DE ROOT / SUDO EN UNA DISTRIBUCION RECIEN INSTALADA
-# PROBADO EN KALI2020.4 Y KALI2021.1
+# PROBADO EN KALI2022.2
 # DESINSTALANDO AUTOARRANQUE
 sudo update-rc.d lanaudit-boot.sh remove
 sudo rm /etc/init.d/lanaudit-boot.sh
