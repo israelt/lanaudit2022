6367a6ed55118185d33b4e6bede4d6dd0d3a9c4b
Israel Torres <israel.torres.gonzalo@gmail.com>
2022-05-26 01:22:57 +0200

    Update enable_autostart.sh
    Update to 2022 Q2 version

--- install/enable_autostart.sh
+++ install/enable_autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # EJECUTAR CON PERMISOS DE ROOT / SUDO EN UNA DISTRIBUCION RECIEN INSTALADA
-# PROBADO EN KALI2020.4 Y KALI2021.1
+# PROBADO EN KALI2022.2
 # INSTALANDO AUTOARRANQUE
 sudo cp /opt/lanaudit/install/lanaudit-boot.sh /etc/init.d/
 sudo chmod +x /etc/init.d/lanaudit-boot.sh
