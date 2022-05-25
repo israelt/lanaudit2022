9b199449488f6a7392d0ebb92afd90a31a31ab86
Israel Torres <israel.torres.gonzalo@gmail.com>
2022-05-26 01:10:58 +0200

    Update lanaudit.sh
    Update to 2022 Q2 version

--- lanaudit.sh
+++ lanaudit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # EJECUTAR CON PERMISOS DE ROOT / SUDO
-# PROBADO EN KALI2020.4 Y KALI2021.1
+# PROBADO EN KALI2022.2
 # SCRIPT DE INICIO
 cd /opt/lanaudit
 sudo python3 /opt/lanaudit/main.py
